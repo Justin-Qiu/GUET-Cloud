e3505e3967e92ea53b20cd05507c16222d8f8348
Zhenyao Qiu <qiuzhenyao@foxmail.com>
2017-03-19 22:50:33 +0800

    Function update

--- webServer/server.py
+++ webServer/server.py
@@ -2,9 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import MySQLdb
+import os
+import os.path
+import time
 
-# 用户登录函数
+'''
+用户登录函数：
+输入参数
+    用户名，口令
+返回值
+    登录成功：姓名，性别，单位，职务，域，文件信息（文件名，大小，修改日期，权限）
+    密码错误：-1
+    用户名不存在：-2
+'''
 def user_log(usr, pwd):
+
     #连接MySQL数据库
     conn = MySQLdb.connect(
             host='localhost', 
@@ -23,7 +35,7 @@ def user_log(usr, pwd):
     # 执行SQL语句
     cur.execute(sql)
     
-    # 获取所有记录列表
+    # 获取用户信息
     results = cur.fetchall()
     for row in results:
         pass_word = row[2]
@@ -51,28 +63,73 @@ def user_log(usr, pwd):
                 area = u'域外'
             
             # 获取所有文件和权限
-            cur.execute("SELECT * FROM files")
-            results_files = cur.fetchall()
             files = []
-            if title == u'校长':
-                colume = 2
-            elif title == u'院长':
-                colume = 3
-            elif title == u'老师':
-                colume = 4
-            elif title == u'学生':
-                colume = 5
-            trans = {'R': u'读', 'W': u'写', 'O': u'操作'}
-            for row in results_files:
-                row[colume]
-                auth = u''
-                for cha in row[colume]:
-                    auth = auth + trans[cha]
-                files.append([row[1], auth])
+            for i in os.walk('files'):
+                i[2].sort()
+                for j in i[2]:
+                
+                    # 获取文件大小
+                    file_size = os.path.getsize('files/%s' % j)
+                    if file_size < 1024:
+                        file_size = str(file_size) + 'B'
+                    elif file_size < (1024 * 1024):
+                        file_size = '%.1f' % (float(file_size)/1024) + 'KB'
+                    elif file_size < (1024 * 1024 * 1024):
+                        file_size = '%.1f' % (float(file_size)/(1024 * 1024)) + 'MB'
+                    elif file_size < (1024 * 1024 * 1024):
+                        file_size = '%.1f' % (float(file_size)/(1024 * 1024 * 1024)) + 'GB'
+                    else:
+                        file_size = '%.1f' % (float(file_size)/(1024 * 1024 * 1024 * 1024)) + 'TB'
+                        
+                    # 获取文件最后修改时间
+                    file_time = os.stat('files/%s' % j).st_mtime
+                    struct_time = time.localtime(file_time)
+                    if struct_time.tm_mon < 10:
+                        mon = '0' + str(struct_time.tm_mon)
+                    else:
+                        mon = str(struct_time.tm_mon) 
+                    if struct_time.tm_mday < 10:
+                        mday = '0' + str(struct_time.tm_mday)
+                    else:
+                        mday = str(struct_time.tm_mday) 
+                    if struct_time.tm_hour < 10:
+                        hour = '0' + str(struct_time.tm_hour)
+                    else:
+                        hour = str(struct_time.tm_hour) 
+                    if struct_time.tm_min < 10:
+                        mins = '0' + str(struct_time.tm_min)
+                    else:
+                        mins = str(struct_time.tm_min)
+                    if struct_time.tm_sec < 10:
+                        sec = '0' + str(struct_time.tm_sec)
+                    else:
+                        sec = str(struct_time.tm_sec)
+                    file_time = (str(struct_time.tm_year) + '-' + 
+                                 mon + '-' + mday + ' ' + hour + ':' + mins + ':' + sec)
+                    
+                    # 从数据库中读取用户权限
+                    cur.execute("SELECT * FROM files WHERE fname = '%s'" % j)
+                    result_file = cur.fetchall()
+                    if title == u'校长':
+                        colume = 2
+                    elif title == u'院长':
+                        colume = 3
+                    elif title == u'老师':
+                        colume = 4
+                    elif title == u'学生':
+                        colume = 5
+                    trans = {'R': u'读', 'W': u'写', 'O': u'操作'}
+                    for row in result_file:
+                        row[colume]
+                        auth = u''
+                        for cha in row[colume]:
+                            auth = auth + trans[cha]
+                        files.append([row[1], file_size, file_time, auth])
             
             # 返回信息
             return name, sex_zh, unit_name, title, area, files
         else:
+        
             # 口令错误
             return -1
        
@@ -81,3 +138,77 @@ def user_log(usr, pwd):
 
     # 关闭数据库连接
     conn.close()
+
+  
+'''
+用户读文件函数：
+输入参数
+    用户名，文件名
+返回值
+    有读写权限：文件内容，1
+    只有读权限：文件内容，-1
+    无读权限：-2
+'''
+def file_read(usr, file_name):
+
+    #连接MySQL数据库
+    conn = MySQLdb.connect(
+            host='localhost', 
+            user='admin', 
+            passwd='123456', 
+            db='project',
+            charset='utf8'
+            )
+            
+    # 创建游标
+    cur = conn.cursor()
+    
+    # SQL查询语句
+    sql = "SELECT * FROM users WHERE loginid = '%s'" % usr
+    
+    # 执行SQL语句
+    cur.execute(sql)
+    
+    # 获取用户信息
+    results = cur.fetchall()
+    for row in results:
+        title = row[6]
+        cur.execute("SELECT * FROM files WHERE fname = '%s'" % file_name)
+        result_file = cur.fetchall()
+        if title == u'校长':
+            colume = 2
+        elif title == u'院长':
+            colume = 3
+        elif title == u'老师':
+            colume = 4
+        elif title == u'学生':
+            colume = 5
+        
+        # 根据用户权限读取文件
+        for fil in result_file:
+            if 'R' in fil[colume] and 'W' in fil[colume]:
+                with open('files/%s' % file_name, 'r') as f:
+                    txt = f.read()
+                return txt, 1
+            elif 'R' in fil[colume]:
+                with open('files/%s' % file_name, 'r') as f:
+                    txt = f.read()
+                return txt, -1
+            else:
+                return -2    
+
+    # 关闭数据库连接
+    conn.close()    
+
+
+'''
+用户写文件函数：
+输入参数
+    用户名，文件名
+返回值
+    有写权限且写入成功：1
+    有写权限但写入失败：-1
+    无写权限：-2
+''' 
+def file_write(usr, file_name):
+    pass
--- webServer/test.py
+++ webServer/test.py
@@ -2,157 +2,49 @@
 # -*- coding: utf-8 -*-
 
 from server import user_log
+from server import file_read
 
-print ''
+print '--------------------------------------------------'
+usr = raw_input('用户名：')
+pwd = raw_input('密码：')
 
-try1 = user_log('1101120705', '123456')
-print '1101120705',
+try1 = user_log(usr, pwd)
+
+print '--------------------------------------------------'
+
 if try1 == -1:
     print '密码错误！'
 elif try1 == -2:
     print '用户名错误！'
 else:
     print '登录成功！'
+    print '--------------------------------------------------'
     for item in try1[:-1]:
         print item + ' ',
     print ''
+    print '--------------------------------------------------'
+
 if try1 != -1 and try1 != -2:
     for i in range(len(try1[-1])):
-        for j in range(2):
+        for j in range(4):
             print try1[-1][i][j],
         print ''
 
-print ''
+print '--------------------------------------------------'
 
-try1 = user_log('1101120706', '654321')
-print '1101120706',
-if try1 == -1:
-    print '密码错误！'
-elif try1 == -2:
-    print '用户名错误！'
-else:
-    print '登录成功！'
-    for item in try1[:-1]:
-        print item + ' ',
-    print ''
 if try1 != -1 and try1 != -2:
-    for i in range(len(try1[-1])):
-        for j in range(2):
-            print try1[-1][i][j],
-        print ''
-
-print ''
-
-try1 = user_log('1101120707', '123456')
-print '1101120707',
-if try1 == -1:
-    print '密码错误！'
-elif try1 == -2:
-    print '用户名错误！'
-else:
-    print '登录成功！'
-    for item in try1[:-1]:
-        print item + ' ',
-    print ''
-if try1 != -1 and try1 != -2:
-    for i in range(len(try1[-1])):
-        for j in range(2):
-            print try1[-1][i][j],
-        print ''
-
-print ''
-
-try1 = user_log('1101120710', '123456')
-print '1101120710',
-if try1 == -1:
-    print '密码错误！'
-elif try1 == -2:
-    print '用户名错误！'
-else:
-    print '登录成功！'
-    for item in try1[:-1]:
-        print item + ' ',
-    print ''
-if try1 != -1 and try1 != -2:
-    for i in range(len(try1[-1])):
-        for j in range(2):
-            print try1[-1][i][j],
-        print ''
-
-print ''
-
-try1 = user_log('1101120711', '123456')
-print '1101120711',
-if try1 == -1:
-    print '密码错误！'
-elif try1 == -2:
-    print '用户名错误！'
-else:
-    print '登录成功！'
-    for item in try1[:-1]:
-        print item + ' ',
-    print ''
-if try1 != -1 and try1 != -2:
-    for i in range(len(try1[-1])):
-        for j in range(2):
-            print try1[-1][i][j],
-        print ''
-
-print ''
-
-try1 = user_log('1101120712', '123456')
-print '1101120712',
-if try1 == -1:
-    print '密码错误！'
-elif try1 == -2:
-    print '用户名错误！'
-else:
-    print '登录成功！'
-    for item in try1[:-1]:
-        print item + ' ',
-    print ''
-if try1 != -1 and try1 != -2:
-    for i in range(len(try1[-1])):
-        for j in range(2):
-            print try1[-1][i][j],
-        print ''
-
-print ''
-
-try1 = user_log('1101120713', '654321')
-print '1101120713',
-if try1 == -1:
-    print '密码错误！'
-elif try1 == -2:
-    print '用户名错误！'
-else:
-    print '登录成功！'
-    for item in try1[:-1]:
-        print item + ' ',
-        print ''
-    print ''
-if try1 != -1 and try1 != -2:
-    for i in range(len(try1[-1])):
-        for j in range(2):
-            print try1[-1][i][j],
-        print ''
-
-print ''
-
-try1 = user_log('1101120714', '123456')
-print '1101120714',
-if try1 == -1:
-    print '密码错误！'
-elif try1 == -2:
-    print '用户名错误！'
-else:
-    print '登录成功！'
-    for item in try1[:-1]:
-        print item + ' ',
-        print ''
-    print ''
-if try1 != -1 and try1 != -2:
-    for i in range(len(try1[-1])):
-        for j in range(2):
-            print try1[-1][i][j],
-        print ''
+    index = raw_input('请输入您要查看的文件序号：')
+    print '--------------------------------------------------'
+    file_name = '文件' + index
+    try2 = file_read(usr, file_name)
+    if try2 == -2:
+        print '您无权阅读该文件！'
+    elif try2[1] == -1:
+        print try2[0],
+        print '--------------------------------------------------'
+        print '您无权修改该文件！'
+    elif try2[1] == 1:
+        print try2[0],
+        print '--------------------------------------------------'
+        print '您可以修改该文件！'   
+    print '--------------------------------------------------' 
